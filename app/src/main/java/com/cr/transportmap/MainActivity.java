06274e5bb6e8766f65ab3443569dae78f237294a
crkim <0201kchm0224@gmail.com>
2022-09-07 19:57:36 +0900

    add kakao api files

--- app/src/main/java/com/cr/transportmap/MainActivity.java
+++ app/src/main/java/com/cr/transportmap/MainActivity.java
@@ -1,9 +1,19 @@
 package com.cr.transportmap;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import com.cr.transportmap.databinding.ActivityMainBinding;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +24,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        mTextView = binding.text;
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
+
 }
