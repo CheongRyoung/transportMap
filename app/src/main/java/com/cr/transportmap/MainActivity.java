02b65b32688061082e69cad6efcdff5803cf27ed
0201kchm0224@gmail.com <0201kchm0224@gmail.com>
2022-09-07 22:50:04 +0900

    complete kakaoMap - setContentView() 누락 해결

--- app/src/main/java/com/cr/transportmap/MainActivity.java
+++ app/src/main/java/com/cr/transportmap/MainActivity.java
@@ -1,32 +1,19 @@
 package com.cr.transportmap;
 
 import android.app.Activity;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import com.cr.transportmap.databinding.ActivityMainBinding;
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
     }
-
 }
